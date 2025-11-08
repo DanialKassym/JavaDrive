288e6e62254a717d5b4e70dbde8f7c205c0866b6
Danial Kassym <danialkassym@gmail.com>
2025-11-08 22:53:00 +0500

    Added maven local and spring security dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.4.0"
 	id("io.spring.dependency-management") version "1.1.6"
-
 }
 
 group = "JavaDrive"
@@ -15,15 +14,15 @@ java {
 }
 
 repositories {
+	mavenLocal()
 	mavenCentral()
 }
-
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-mail")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	//implementation("org.springframework.boot:spring-boot-starter-security")
-	//testImplementation("org.springframework.security:spring-security-test")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	testImplementation("org.springframework.security:spring-security-test")
 	implementation ("org.springframework.data:spring-data-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
