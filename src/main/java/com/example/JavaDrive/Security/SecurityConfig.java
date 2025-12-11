18359f82b79685be65fbf8d316c1cb80646b5875
Danial Kassym <danialkassym@gmail.com>
2025-12-11 22:09:43 +0500

    Changed SecurityConfig,adding security filters with public and swagger endpoints, future secure endpoint and making session stateless for jwt integration. Added DaoAuthenticationProvider, passwordEncoder and authManager

--- src/main/java/com/example/JavaDrive/Security/SecurityConfig.java
+++ src/main/java/com/example/JavaDrive/Security/SecurityConfig.java
@@ -1,26 +1,54 @@
 package com.example.JavaDrive.Security;
 
+import com.example.JavaDrive.Users.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final UserService userService;
+    public SecurityConfig(UserService userService) {
+        this.userService = userService;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf((csrf -> csrf.disable()))
+                .cors((cors -> cors.disable()))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/unsecure","/swagger-ui/**", "/swagger-ui.html", "/swagger-resources/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/unsecure","/swagger-ui/**", "/swagger-ui.html",
+                                "/swagger-resources/**", "/v3/api-docs/**","/auth","/reg").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
-                .formLogin(Customizer.withDefaults());
-
+                .sessionManagement((session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)));
         return http.build();
+    }
+
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider daoAuthProvider = new DaoAuthenticationProvider();
+        daoAuthProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthProvider.setUserDetailsService(userService);
+        return daoAuthProvider;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
