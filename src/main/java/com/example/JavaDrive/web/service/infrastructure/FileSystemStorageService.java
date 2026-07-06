62b25cbde3d0fcc0c23b85689164edb9ded91df7
Danial Kassym <danialkassym@gmail.com>
2026-07-06 12:27:31 +0500

    Refactor: Unused loadAsResource and Load methods deleted from FileSystemStorageService

--- src/main/java/com/example/JavaDrive/web/service/infrastructure/FileSystemStorageService.java
+++ src/main/java/com/example/JavaDrive/web/service/infrastructure/FileSystemStorageService.java
@@ -2,16 +2,12 @@ package com.example.JavaDrive.web.service.infrastructure;
 
 import com.example.JavaDrive.config.StorageProperties;
 import com.example.JavaDrive.exception.StorageException;
-import com.example.JavaDrive.exception.StorageFileNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -48,25 +44,6 @@ public class FileSystemStorageService {
             }
         } catch (IOException e) {
             throw new StorageException("Failed to store file.");
-        }
-    }
-    public Path load(String filename) {
-        return rootLocation.resolve(filename);
-    }
-
-    public Resource loadAsResource(String filename) {
-        try {
-            Path file = load(filename);
-            Resource resource = new UrlResource(file.toUri());
-            if (resource.exists() || resource.isReadable()) {
-                return resource;
-            } else {
-                throw new StorageFileNotFoundException(
-                        "Could not read file: " + filename);
-
-            }
-        } catch (MalformedURLException e) {
-            throw new StorageFileNotFoundException("Could not read file: " + filename);
         }
     }
 
